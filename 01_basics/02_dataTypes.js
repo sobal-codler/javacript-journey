6d9dc66533b8b5f23a05084a87676e753ca6808c
sobal-codler <112180377+sobal-codler@users.noreply.github.com>
2025-03-17 12:24:14 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" // Treat all code in JS as newer version
+
+// alert (3+3); we can't use alert in node, it is avaible but syntax is changed, easily available in browser.
+
+console.log(3 
+    + 3)    // Code readability should be high
+
+console.log("Sobal")
+
+let name = "sobal"
+let age = 20
+let isLoggedIn = false
+let stat = ""
+
+// number => 2 to power 53
+// bigInt
+// string = ""
+// boolean => true/false
+// null => standalone value  ====> data type of null is 'object'
+// undefined => not defined but exist
+// symbol => unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
