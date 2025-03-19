3fc5f5086147a065fca896d1e68675dfa892076d
sobal-codler <112180377+sobal-codler@users.noreply.github.com>
2025-03-19 07:43:05 +0000

    Comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// simple comparisons
+
+
+
+//    console.log(1 > 2);
+//    console.log(1 < 2);
+//    console.log(3 == 5);
+//    console.log(4 >= 5);
+//    console.log(2 <= 4);
+//    console.log(5 != 5);
+
+//console.log( "2" > 1);
+//console.log( "02" > 1);
+//console.log(null > 0); not predictable ==> avoid
+//console.log(null == 0); //
+//console.log(null >= 0); // it convert null 0 then compare
+//console.log(null <= 0); // it convert null 0 then compare
+
+//console.log(undefined == 0); //false
+//console.log(undefined > 0);  //false
+//console.log(undefined < 0);  // false
+
