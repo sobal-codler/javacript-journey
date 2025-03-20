408fb52173b0fb412b015503b5ef5a0a2ec7dcc4
sobal-codler <112180377+sobal-codler@users.noreply.github.com>
2025-03-20 08:09:46 +0000

    strings and its methods

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -26,9 +26,26 @@ console.log(gameName.toUpperCase());    // convert the copy to uppercase
 console.log(gameName.charAt(3));    // we can check at what position which character is
 console.log(gameName.indexOf('n')); // we check the position of any character
 
-const newString = gameName.substring(0, 4)      // start from 0 index and end at 3, it doesn't give the
+const newString = gameName.substring(0, 4)      // start from 0 index and end at 3, it doesn't give the  
+//      0 indicate the starting position and other value which is written 4 indicate end value
+console.log(newString);     
 
+    // output => Warz  it retuning from 0 index to 3 index
 
+const anotherSting = gameName.slice(-8, 3);     // it same like substring but it is also can pas -ive value
+// which then act in reverse and return val from last 
 
+console.log(anotherSting);
+
+const newStringOne = "    sobal    ";       // extra space
+console.log(newStringOne);
+console.log(newStringOne.trim());       // remove extra space
+// read mdn about trim
+
+const url = "https:www.google%20idon'tknow"        // browser add  %20 to remove empty space
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes("war"));
 
 
