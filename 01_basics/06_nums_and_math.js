5360edf1b25cc36d992bb3a120567e6fee5aece7
Sobal Naseeb <112180377+sobal-codler@users.noreply.github.com>
2025-03-22 13:00:22 +0000

    Numbers and Math descriptions

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,14 @@
+const score = 400;
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);       // convert into objects and prototype
+
+//console.log(balance.toString());
+
+//console.log(balance.toFixed(2));  // return mentiond strength of number
+
+const otherNumber = 123.3454
+
+console.log(otherNumber.toPrecision(4));
+
