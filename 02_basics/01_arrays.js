bae40b141b07ed14f9a50481a26d4b6a035ef23e
Sobal Naseeb <112180377+sobal-codler@users.noreply.github.com>
2025-03-25 08:16:48 +0000

    Arrays Explanation

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,1 +1,12 @@
 // arrays
+
+// The Array object, as with arrays in other programming languages, enables storing a collection of multiple items under a single variable name, and has members for performing common array operations.
+/*
+Core Characteristics
+-> resizeable & can contain mix of diff data types
+-> JS arrays are not associative arrays, so can't be accessed arbitrary (only non negative indexes)
+-> Are zero indexed & length pro mused be minus 1.
+-> JS copy operations create shallow Shallow copies (same reference) rather than deep deep copies ()
+*/
+
+
