6abe676d461035484f163a093317571d55580039
Sobal Naseeb <112180377+sobal-codler@users.noreply.github.com>
2025-03-28 08:01:51 +0000

    Objects Investigation

--- 02_basics/03_ojects.js
+++ 02_basics/03_ojects.js
@@ -1,1 +1,19 @@
 // Singleton
+// if we make object by constructor it create singleton (unique 'i guess')
+// Object literals => When we make other that cons the (its multiple instances created)
+
+// Object literals:
+
+const JsMaster = {
+    name: 'Sobal',
+    age: 16,
+    location: 'Islamabad',
+    email: 'sobalreeb@gmail.com',
+    isLoggedIn: true,
+    lastLoginDays: ['Monday', 'Saturday']
+}
+
+// access methods => 
+
+console.log(JsMaster.email);
+console.log(JsMaster['email']);
