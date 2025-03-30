4543e32af4dddd35b05b5a0217dfac46f065fa74
Sobal Naseeb <112180377+sobal-codler@users.noreply.github.com>
2025-03-30 16:49:01 +0000

    Objects more details

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,11 @@
+const tinderUser = new Object()     // singleton object
+const tinderUser1 = {}              // Non singleton object
+
+tinderUser.id = "1234as"
+tinderUser.name = "Tech Savy"
+tinderUser.isLogedIn = false
+
+console.log(tinderUser);
+
+
+
