e7a8591d1e5fc999663a1dedc84235c7da46c0de
Sobal Naseeb <112180377+sobal-codler@users.noreply.github.com>
2025-03-30 16:59:45 +0000

    Objects more detailing

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -5,7 +5,31 @@ tinderUser.id = "1234as"
 tinderUser.name = "Tech Savy"
 tinderUser.isLogedIn = false
 
-console.log(tinderUser);
+//  console.log(tinderUser);
+
+const regularObj = {
+    email: "someone@gmail.com",
+    fullname: {
+        username: {
+            firstname: "some",
+            lastname: "one"
+        }
+    }
+}
+
+//  console.log(regularObj.fullname.username.firstname);
 
 
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+//     how to combine objects 
+const combine = {obj1, obj2}
+//  console.log(combine);
+//  output  { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } }
+
+const combining = Object.assign({}, obj1, obj2)
+console.log(combining);
+//  output  { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+// first val is target(save all sources), second val is sources
 
