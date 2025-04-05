12a0859b542de8d76a223527513afd81143ee3c7
Sobal Naseeb <112180377+sobal-codler@users.noreply.github.com>
2025-04-05 08:56:38 +0000

    scopes definition

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,10 @@
+// var, let , const
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+//console.log(a);   // output => a is not defined, blocked scope  
+//console.log(b);   // output => b is not defined, blocked scope  
+console.log(c);     // global scope
