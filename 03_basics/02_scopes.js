f02aa468e67b80948b57f902565359b3d0b95e2e
Sobal Naseeb <112180377+sobal-codler@users.noreply.github.com>
2025-04-06 14:29:28 +0000

    Scopes detailing

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,8 +7,41 @@ if (true) {
 
 //console.log(a);   // output => a is not defined, blocked scope  
 //console.log(b);   // output => b is not defined, blocked scope  
-console.log(c);     // global scope
+//console.log(c);     // global scope
 
 //scope in window & node ?
 // both scopes are different
 //  In browsers, the window object serves as the global context, while Node. js uses the global object
+
+function one(){
+    const kakaname = "puchi"
+
+    function two(){
+        const website = "Youtube"
+        //console.log(kakaname);
+    }
+    //console.log(website);
+    //two()
+    
+}
+one()
+
+// ===============Interesting=============
+
+console.log(addone(5));
+
+
+function addone(num){
+    return num + 1
+}
+//  output => 6
+// addone(5)
+
+// In this case there won't be any error due to hoisting
+
+//  console.log(addtwo(5));     // it will give error => cannot access before initialization
+const addtwo = function(num){
+    return num + 2
+}
+addtwo(5)
+// In this case there won't be any error due to hoisting
