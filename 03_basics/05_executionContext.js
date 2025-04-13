3644a792dfcdbeab04156b7d995f530a6686d436
Sobal Naseeb <112180377+sobal-codler@users.noreply.github.com>
2025-04-13 08:58:15 +0000

    Execution

--- 03_basics/05_executionContext.js
+++ 03_basics/05_executionContext.js
@@ -1,1 +1,12 @@
 // {} this is execution context 
+// There are three types of execution context
+// 1. Global Execution Context
+// 2. Function Execution Context
+// 3. Eval Execution Context (this optional only in some environment => global)
+
+
+// there are two major phases
+// 1. Memory Creation Phase (MCP) => Creation Phase
+// 2. Execution Phase
+
+
