8e5d86837e12b17874a50f102e9a246ed6ded33b
Sobal Naseeb <112180377+sobal-codler@users.noreply.github.com>
2025-04-14 07:17:24 +0000

    Exec

--- 03_basics/05_executionContext.js
+++ 03_basics/05_executionContext.js
@@ -1,8 +1,10 @@
+// Always remeber JavaScript is single threaded
+
 // {} this is execution context 
 // There are three types of execution context
 // 1. Global Execution Context
 // 2. Function Execution Context
-// 3. Eval Execution Context (this optional only in some environment => global)
+// 3. Eval Execution Context (e.g in mongo, add as global property, (this optional only in some environment => global))
 
 
 // there are two major phases
