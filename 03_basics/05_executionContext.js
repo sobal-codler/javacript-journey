fb0b4275a31b3ce1babb4f0c1f2e0659c9991e3c
Sobal Naseeb <112180377+sobal-codler@users.noreply.github.com>
2025-04-15 05:53:52 +0000

    Execution def

--- 03_basics/05_executionContext.js
+++ 03_basics/05_executionContext.js
@@ -11,4 +11,11 @@
 // 1. Memory Creation Phase (MCP) => Creation Phase
 // 2. Execution Phase
 
+// There are two types of execution contexts: global and function. 
+// The global execution context is created when a JavaScript script first starts to run, 
+// and it represents the global scope in JavaScript. A function execution context is created 
+// whenever a function is called, representing the function's local scope.
 
+// Execution Context is like a big box or container where all JavaScript code is executed. 
+// Also, before continuing reading remember that JavaScript is a synchronous single-threaded 
+// language. Single-threaded means that JavaScript can execute one command at a time.
