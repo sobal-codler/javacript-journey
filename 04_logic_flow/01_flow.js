42c9648b9c9318dd615ded8839f59c13a72e27db
Sobal Naseeb <112180377+sobal-codler@users.noreply.github.com>
2025-04-15 06:27:23 +0000

    learn flow

---
+++ 04_logic_flow/01_flow.js
@@ -0,0 +1,21 @@
+// if(agr yeh true ho ga) {
+//     phr yeh code run ho ga
+// }
+
+// comparison operators
+// < , > , <= , >= , == , != , === , !==
+
+const temperature = 41;
+
+if( temperature < 50) {
+    console.log('Less than 50');
+}
+
+const score = 200;
+
+if(score > 100){
+    let power = 'fly';
+    console.log(`User power: ${power}`);    
+}
+console.log(`User has power of ${power}`);
+
