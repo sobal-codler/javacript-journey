1dcb724a1d683afeee085a91bf18bc8b6e75069d
Sobal Naseeb <112180377+sobal-codler@users.noreply.github.com>
2025-04-15 06:17:16 +0000

    flow

---
+++ 04_logic_flow/one.js
@@ -0,0 +1,12 @@
+// if(agr yeh true ho ga) {
+//     phr yeh code run ho ga
+// }
+
+// comparison operators
+// < , > , <= , >= , == , != , === , !==
+
+const temperature = 41;
+
+if( temperature < 50) {
+    console.log('Less than 50');
+}
