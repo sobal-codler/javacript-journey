b31cce54ca1f41c151b9bdc8a2ec61a9bd06ccce
Sobal Naseeb <112180377+sobal-codler@users.noreply.github.com>
2025-05-02 07:08:40 +0000

    switch prac

--- 04_logic_flow/02_switch.js
+++ 04_logic_flow/02_switch.js
@@ -1,13 +1,27 @@
 // Basic syntax of a switch statement
-switch (expression) {
-    case value1:
-        // Code to execute if expression === value1
+// switch (expression) {
+//     case value1:
+//         // Code to execute if expression === value1
+//         break;
+//     case value2:
+//         // Code to execute if expression === value2
+//         break;
+//     // Add more cases as needed
+//     default:
+//         // Code to execute if no case matches
+//         break;
+// }
+
+const foo = 1;
+switch (foo) {
+    case 1:
+        console.log("foo is 1");
         break;
-    case value2:
-        // Code to execute if expression === value2
+    case 2:
+        console.log("foo is 2");
         break;
-    // Add more cases as needed
     default:
-        // Code to execute if no case matches
+        console.log("foo is neither 1 nor 2");
         break;
+            
 }
