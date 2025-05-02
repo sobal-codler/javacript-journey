9404aca8203499fd46172479d21497dda575d2e2
Sobal Naseeb <112180377+sobal-codler@users.noreply.github.com>
2025-05-02 07:05:04 +0000

    Switch syntax

---
+++ 04_logic_flow/02_switch.js
@@ -0,0 +1,13 @@
+// Basic syntax of a switch statement
+switch (expression) {
+    case value1:
+        // Code to execute if expression === value1
+        break;
+    case value2:
+        // Code to execute if expression === value2
+        break;
+    // Add more cases as needed
+    default:
+        // Code to execute if no case matches
+        break;
+}
