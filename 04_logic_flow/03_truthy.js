39102e49c21bf1a6b2e8ce637d2381d0cbbaa813
Sobal Naseeb <112180377+sobal-codler@users.noreply.github.com>
2025-05-04 15:48:41 +0000

    Truthy & Falsy

--- 04_logic_flow/03_truthy.js
+++ 04_logic_flow/03_truthy.js
@@ -17,3 +17,18 @@
 // when encountered in a Boolean context. All values are truthy unless 
 // they are defined as falsy. That is, all values are truthy 
 // except false, 0, -0, 0n, "", null, undefined, NaN, and document.all.
+
+
+// if (true);
+// if ({});
+// if ([]);
+// if (42);
+// if ("0");
+// if ("false");
+// if (new Date());
+// if (-42);
+// if (12n);
+// if (3.14);
+// if (-3.14);
+// if (Infinity);
+// if (-Infinity);
